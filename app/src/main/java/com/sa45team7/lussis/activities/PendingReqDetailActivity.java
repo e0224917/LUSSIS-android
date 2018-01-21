b4102a43ada8edd5db591e7b153d1d2a0fd8b606
e0224947 <e0224947@u.nus.edu>
2018-01-21 20:13:58 +0800

    ui

---
+++ app/src/main/java/com/sa45team7/lussis/activities/PendingReqDetailActivity.java
@@ -0,0 +1,15 @@
+package com.sa45team7.lussis.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sa45team7.lussis.R;
+
+public class PendingReqDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_req_detail);
+    }
+}
